2910a0ff3c9a9d7184133ac002d24af8c2732fd2
ayunas <ayunas@gmail.com>
2019-04-25 22:49:55 -0500

    rendered a basic header with a <Loader>

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Loader from 'react-loader-spinner';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Loader type="Grid" color="white" />  
+          <h2>Star Wars SWAPI </h2>
       </header>
     </div>
   );
